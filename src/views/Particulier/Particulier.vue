0d92c697df8917c4ee5ec0733be65cccc3465133
StoicBug <outidrarinebachir@gmail.com>
2023-04-05 12:09:15 +0000

    particulier UI structure

--- src/views/Particulier/Particulier.vue
+++ src/views/Particulier/Particulier.vue
@@ -72,18 +72,17 @@
     </header>
     <div class="mx-auto max-w-screen-xl px-4 py-8">
         <ul class="flex border-b border-gray-100">
+
             <li class="flex-1">
                 <a class="relative block p-4" href="">
                     <span class="absolute inset-x-0 -bottom-px h-px w-full bg-pink-600"></span>
-
                     <div class="flex items-center justify-center gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex-shrink-0 text-gray-500" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                         </svg>
 
-                        <span class="text-sm font-medium text-gray-900"> Settings </span>
+
+                        <span class="text-sm font-medium text-gray-900"> Les Demandes </span>
                     </div>
                 </a>
             </li>
@@ -91,13 +90,12 @@
             <li class="flex-1">
                 <a class="relative block p-4" href="">
                     <div class="flex items-center justify-center gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex-shrink-0 text-gray-500" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                         </svg>
 
-                        <span class="text-sm font-medium text-gray-900"> Messages </span>
+
+                        <span class="text-sm font-medium text-gray-900"> Offers </span>
                     </div>
                 </a>
             </li>
@@ -111,7 +109,7 @@
                                 d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
                         </svg>
 
-                        <span class="text-sm font-medium text-gray-900"> Archive </span>
+                        <span class="text-sm font-medium text-gray-900"> Historic </span>
                     </div>
                 </a>
             </li>
@@ -122,10 +120,10 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex-shrink-0 text-gray-500" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                         </svg>
 
-                        <span class="text-sm font-medium text-gray-900"> Notifications </span>
+                        <span class="text-sm font-medium text-gray-900"> Settings </span>
                     </div>
                 </a>
             </li>
@@ -137,16 +135,16 @@
                 <thead>
                     <tr>
                         <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                            Name
+                            Type
                         </th>
                         <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                            Date of Birth
+                            Date Effectue
                         </th>
                         <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                            Role
+                            Transporteur
                         </th>
                         <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                            Salary
+                            Prix
                         </th>
                         <th class="px-4 py-2"></th>
                     </tr>
@@ -155,10 +153,10 @@
                 <tbody class="divide-y divide-gray-200">
                     <tr>
                         <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                            John Doe
+                            Aller Simple
                         </td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">CTM</td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
                         <td class="whitespace-nowrap px-4 py-2">
                             <a href="#" class="inline-block rounded bg-primary px-4 py-2 text-xs font-medium text-white" @click="openModal()">
@@ -169,10 +167,10 @@
 
                     <tr>
                         <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                            Jane Doe
+                            Aller Simple
                         </td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
-                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Farasha</td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
                         <td class="whitespace-nowrap px-4 py-2">
                             <a href="#" class="inline-block rounded bg-primary px-4 py-2 text-xs font-medium text-white">
@@ -183,10 +181,10 @@
 
                     <tr>
                         <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                            Gary Barlow
+                            Circuit
                         </td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">AHmed</td>
                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
                         <td class="whitespace-nowrap px-4 py-2">
                             <a href="#" class="inline-block rounded bg-primary px-4 py-2 text-xs font-medium text-white ">
